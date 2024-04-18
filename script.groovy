ecd2126a2ac1af561bf389086ee844aff6831a06
Ahmed Magdi <a.magdi888@gmail.com>
2024-04-18 13:31:55 +0200

    add the code

---
+++ script.groovy
@@ -0,0 +1,30 @@
+#!/usr/bin/env groovy
+
+def BuildImage() {
+    withCredentials([usernamePassword(credentialsId :'DockerHub',usernameVariable :'USER',passwordVariable :'PASSWORD')]){
+       sh '''
+       tag=`git log --format="%H" -n 1 | cut -c 1-7`
+       docker build -t amagdi888/nodeapp:${tag}${BUILD_ID} .
+       echo $PASSWORD | docker login -u $USER --password-stdin
+       docker push amagdi888/nodeapp:${tag}${BUILD_ID}
+       '''
+    }
+
+}
+
+
+def DeployToProduction() {
+    withKubeConfig([credentialsId: 'K8S']) {
+                        sh '''
+                        tag=`git log --format="%H" -n 1 | cut -c 1-7`
+                        helm upgrade --install -n blue-tide --set image.tag=${tag}${BUILD_ID} nodeapp ./helm
+                        helm install -n blue-tide my-mongodb oci://registry-1.docker.io/bitnamicharts/mongodb
+                        helm install -n blue-tide my-memcached oci://registry-1.docker.io/bitnamicharts/memcached
+                        helm install -n blue-tide my-rabbitmq oci://registry-1.docker.io/bitnamicharts/rabbitmq
+                        helm install -n blue-tide my-redis oci://registry-1.docker.io/bitnamicharts/redis
+
+                        '''
+                    }
+}
+
+return this
